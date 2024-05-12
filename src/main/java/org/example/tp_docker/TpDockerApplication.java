432c99e72173c507925b2135c15588b2efd93bb6
Anaïs Bernard <anais.mauricette.bernard@gmail.com>
2024-05-12 12:30:19 +0200

    TP_DOCKER

---
+++ src/main/java/org/example/tp_docker/TpDockerApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpDockerApplication.class, args);
+	}
+
+}
